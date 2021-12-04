1104547ef953bcfa5f9a1f4c7947029a72d75a49
lorinalexd@gmail.com <lorinalexd@gmail.com>
2021-12-04 14:42:16 -0500

    initial upload

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE vvr (
+  id INT PRIMARY KEY,
+  name TEXT,
+  address TEXT,
+  city TEXT,
+  country TEXT,
+  postalcode TEXT,
+  categories TEXT,
+  facebookpageurl TEXT,
+  phones TEXT,
+  pricerange TEXT,
+  province TEXT,
+  twitter TEXT,
+  websites TEXT
+);
